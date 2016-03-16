e8fa94f6f5fb439f1c74bf0a0b98ce8ee00104ec
ernesto-pm <ernesto@itia.mx>
2016-03-16 12:49:14 -0600

    Checando si cambio el layout3

--- resources/views/clients.blade.php
+++ resources/views/clients.blade.php
@@ -13,5 +13,15 @@
                     <input type="text" name="name" id="client-name" class="form-control">
                 </div>
             </div>
+
+
+            <div class="form-group">
+                <div class="col-sm-offset-3 col-sm-6">
+                    <button type="submit" class="btn btn-default">
+                        <i class="fa fa-plus"></i> Add Task
+                    </button>
+                </div>
+            </div>
         </form>
+    </div>
 @endsection
