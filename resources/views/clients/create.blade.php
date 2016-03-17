0b52e132c7f84aa6b71b0d0c9db87b3903327b52
ernesto-pm <ernesto@itia.mx>
2016-03-16 18:31:52 -0600

    Añadiendo y probando illumiate

--- resources/views/clients/create.blade.php
+++ resources/views/clients/create.blade.php
@@ -6,4 +6,22 @@
     <p class="lead">Añade un nuevo usuario. </p>
     <hr>
 
+    {!! Form::open([
+    'route' => 'tasks.store'
+]) !!}
+
+    <div class="form-group">
+        {!! Form::label('title', 'Title:', ['class' => 'control-label']) !!}
+        {!! Form::text('title', null, ['class' => 'form-control']) !!}
+    </div>
+
+    <div class="form-group">
+        {!! Form::label('description', 'Description:', ['class' => 'control-label']) !!}
+        {!! Form::textarea('description', null, ['class' => 'form-control']) !!}
+    </div>
+
+    {!! Form::submit('Create New Task', ['class' => 'btn btn-primary']) !!}
+
+    {!! Form::close() !!}
+
 @endsection
