2bb024cdbe88179809464a2c553e9234df6499b8
ernesto-pm <ernesto@itia.mx>
2016-03-16 18:55:58 -0600

    asdas

--- config/app.php
+++ config/app.php
@@ -147,6 +147,7 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
+        Collective\Html\HtmlServiceProvider::class,
         /*
          * Application Service Providers...
          */
@@ -155,7 +156,7 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
-        Collective\Html\HtmlServiceProvider::class,
+
 
 
     ],
@@ -203,8 +204,8 @@ return [
         'URL'       => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View'      => Illuminate\Support\Facades\View::class,
-        'Form' => laravelcollective\html\src\FormFacade::class,
-        'Html' => laravelcollective\html\src\HtmlFacade::class,
+        'Form'      => Collective\Html\FormFacade,
+        'Html'      => Collective\Html\HtmlFacade,
 
     ],
 
